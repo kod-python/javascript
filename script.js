0285b33e5cb704513bbd19c54b1130cfe56409eb
kod-python <kelvinlik0245@gmail.com>
2023-12-05 12:19:20 +0000

    jav script

---
+++ script.js
@@ -0,0 +1,39 @@
+// for text
+console.log("come for me");
+
+
+// for numbers
+console.log(10 + 3);
+
+
+// variables
+let you ="kod"
+console.log(you);
+
+
+// for boolean
+console.log(true);
+console.log(false);
+
+// for arrays
+let arr=[1,2,3,4,5];
+console.log(arr);
+console.log([9,8,7]);
+
+
+// for objects
+console.log({name:"kod", age:20});
+
+
+// for warning
+console.warn("this is a warning");
+
+
+// for erros
+
+console.error("error meaasge");
+
+
+// for clear
+
+console.clear()
---
+++ variable.js
@@ -0,0 +1,14 @@
+// variables {keyword,name,value}
+
+
+// 3 keywords var,let.const
+
+
+// name: could be anything of your choice
+// value: coild be anything of your choice
+// value: srings, numbers, booleans, arrays, objects, null, undefined, symbols
+
+
+
+var blackhat = "stanley";
+console.log(blackhat);

63e375aa5377973c906c48fc86b8c62d273f2f04
kod-python <kelvinlik0245@gmail.com>
2023-12-06 11:32:48 +0000

    java add

--- variable.js
+++ variable.js
@@ -12,3 +12,39 @@
 
 var blackhat = "stanley";
 console.log(blackhat);
+
+
+let name = "deviper,";
+let age = " 20"
+let myschool = "H.T.U.";
+let myhomewotn = "shia.";
+let dreamjob = "cyber secuirity expert";
+
+
+console.log("my name is "+name + "i am"+age +"years old" + "i completed" +myschool + "i come from"+myhomewotn +"in the vlta region" + "my dream job is to be a"+dreamjob +"in the future");
+
+name = "kod"
+age = 26
+
+
+console.log(`my name is ${name} and my age is ${age}. i completed ${myschool} and i comoe from ${myhomewotn} and my dream job is to become a ${dreamjob} in the near future`)
+
+
+// naming conventions
+
+// camel case
+let myOldAge = 9
+
+// snake case
+
+let my_old_age = 10
+
+// pascal case
+let MyoldAge = 12
+
+
+// calculations
+
+
+
+

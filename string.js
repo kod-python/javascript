586c0f167cb46bc274d5f5a09b6e71675ebb136a
kod-python <kelvinlik0245@gmail.com>
2024-01-16 11:14:07 +0000

    java script

---
+++ string.js
@@ -0,0 +1,21 @@
+// to get the legnt of a string
+let firstword = "hello world"
+console.log(firstword.length)
+
+// to turn the string to uppercase
+console.log(firstword.toUpperCase())
+
+// return character at a particular index
+console.log(firstword.charAt(10))
+
+// string a particular value
+console.log(firstword.substring(6,5))
+
+// lenght of string it should print
+let p = "dshgdksdksd dsdsldjlsnljdnls sdskdbskd sdkskdsdskdsdlsdsdll dsskddsdsnslndls sldsldnlsndlsdl dlsldlsd"
+console.log(p.substring(0,40))
+
+// replacing name
+let allNames = "kod deviper"
+console.log(allNames.replace("kod","python"))
+

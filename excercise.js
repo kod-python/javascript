b05d0d262310295aecde5cce3596e82ff37db101
kod-python <kelvinlik0245@gmail.com>
2024-01-24 12:16:19 +0000

    java readd

--- excercise.js
+++ excercise.js
@@ -18,4 +18,44 @@ console.log(myArray.hobbies)
 
 for (let i = 0; i < myArray.length; i += 1) {
   console.log(myArray[i]);
+};
+
+
+
+
+
+// Q1
+ function greeting(name){
+  console.log(`hello, ${name}`)
 }
+
+greeting("kod")
+
+// Q2
+function sumArray(numbers){
+ const sum = numbers.reduce((acc, num) => acc + num,0)
+ console.log(`The sum of the array is:${sum}`);
+}
+
+let my_array_number =[9,8,8,7,2,3,3]
+sumArray(my_array_number)
+
+
+// Q3
+function calculateRectangleArea(l,w){
+  area = (l*w)
+ console.log(`${area}cm`)
+}
+
+
+
+calculateRectangleArea(2,4)
+
+
+
+// Q4
+function calculatePower(a,b){
+
+  console.log(Math.pow(a,b));
+}
+calculatePower(4,3)
--- functions.js
+++ functions.js
@@ -19,3 +19,73 @@ function rizz(name){
 rizz("him")
 
 
+function sub(a,b){
+    return a-b;
+}
+
+console.log(sub(6,1));
+
+
+// function checkRegister(user){
+//     return user + " has registered "
+// }
+
+// console.log(checkRegister("kod"));
+
+
+function checkRegister(user){
+   if(!user){
+    user = "Bot"
+   }
+
+
+}
+
+console.log(checkRegister());
+
+
+
+function enter_number(number){
+    if(!number){
+     number = 0
+    }
+ 
+ }
+ 
+ console.log(enter_number());
+
+
+ const car={
+    color:"red",
+    engine:"v12",
+ }
+
+
+ function my_cars(car){
+    return `my cars color is ${car.color} and it has ${car.engine} engine`
+ }
+ console.log(my_cars(car))
+
+
+ const person1 = {
+    Name:"kod",
+    age:23
+ }
+
+ const person2 = {
+    Name:"joy",
+    age:20
+ }
+ const person3 = {
+    Name:"peace",
+    age:18
+ }
+
+
+ function myPerson(who){
+    return `please tell me about your self. MY name is ${who.Name} and am ${who.age} years old`
+ }
+ console.log(myPerson(person1))
+ console.log(myPerson(person2))
+ console.log(myPerson(person3))
+
